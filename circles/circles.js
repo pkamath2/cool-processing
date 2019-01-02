5d67b4ddc8ac964d76fae1f74a9e679abbb6e7bb
pkamath2 <git.pkamath2@gmail.com>
2019-01-02 23:45:16 +0800

    Colors

--- circles/circles.js
+++ circles/circles.js
@@ -3,10 +3,10 @@ var top = 50;
 var left = 350;
 var i =0;var j =0;
 function setup(){
-    const canvas = createCanvas(1000, 1000);
+    const canvas = createCanvas(1000, 1200);
     canvas.position(left, top);
     background('#fffaf0');
-    frameRate(6)
+    frameRate(60)
     //noLoop();
 }
 
@@ -24,10 +24,16 @@ function draw(){
 }
 
 function drawSquiggles(x, y, r, level, f){
-    fill('black');
+    // fill('black');
+    noStroke();
     var prevPoint = 0;
-    for(var ang=0;ang<2*TWO_PI;ang+=TWO_PI/150){
-        arc(x, y, r + level * Math.cos(ang * 12 - 2 * Math.PI / 3) * 32, r + level * Math.cos(ang * 12 - 2 * Math.PI / 3)  *32, prevPoint, ang, PIE);
+    for(var ang=0;ang<2*TWO_PI;ang+=TWO_PI/300){
+        var ar = r + level * Math.cos(ang * 12 - 2 * Math.PI / 3) * 100;
+        var arColor = 60 + Math.floor((ar)*300/50);
+        var hsl = 'hsl('+arColor+','+Math.floor(ar)+'%,'+level*100+'%)';
+        console.log(level, hsl)
+        fill(color(hsl));
+        arc(x, y, ar, ar, prevPoint, ang, PIE);
         prevPoint = ang;
     }
     
